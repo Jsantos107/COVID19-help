a347c89315e6af1ef247c2e6128fee70f6555264
github username <github email address>
2020-03-24 17:46:16 -0600

    getting map to append to page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import WorldMap from './components/WorldMap'
  class App extends Component {
 
   render(){
     return (
       <div className="App">
         <header className='header'>
-          <img src='./CovidLogo.png' alt='covid-19HELP' />
+          <img src='./WhiteCovidLogo.png' alt='covid-19HELP' />
           <ul className='nav-bar'>
             
           </ul>
         </header>
         <main className='main'>
-          <h2>Something</h2>
+          <WorldMap />
         </main>
       </div>
     );
---
+++ src/components/WorldMap.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { GoogleApiWrapper, InfoWindow, Marker, Map } from 'google-maps-react';
+
+const mapStyles = {
+  width: '50%',
+  height: '50%',
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+    <div className='map'>
+        <h1>Map of number infected</h1>
+        <Map
+            google={this.props.google}
+            zoom={3}
+            style={mapStyles}
+            initialCenter={{
+            lat: 38.8267,
+            lng: -105.7821
+            }}
+        />
+    </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+//   apiKey: 'YOUR API KEY HERE'
+})(MapContainer);
