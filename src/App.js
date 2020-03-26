aa01d9b9241f2ab33e4b2f0cfa4a34cf3e618253
github username <github email address>
2020-03-26 10:36:36 -0600

    Getting route to work starting on maps page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import WorldMap from './components/WorldMap'
-import SideCountries from './components/SideCountries'
+import WorldMap from './components/WorldMap';
+import SideCountries from './components/SideCountries';
 import CountryInfo from './components/CountryInfo';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
  class App extends Component {
    state ={
      covidInfo:[],
@@ -23,18 +24,24 @@ import CountryInfo from './components/CountryInfo';
 
   render(){
     return (
-      <div className="App">
-        <header className='header'>
-          <img src='./CenterCovidLogo.png' alt='covid-19HELP' />
-        </header>
-        <main className='main'>
-          <SideCountries covidInfo={this.state.covidInfo} showCountry={this.showCountry}/>
-          <WorldMap />
-          {this.state && this.state.selectedCountry &&
-            <CountryInfo country={this.state.selectedCountry}/>
-          }
-        </main>
-      </div>
+      <Router>
+        <div className="App">
+            <header className='header'>
+              <img src='./CenterCovidLogo.png' alt='covid-19HELP' />
+            </header>
+            <main className='main'>
+              <Switch>
+                <Route exact path='/map'>
+                <SideCountries covidInfo={this.state.covidInfo} showCountry={this.showCountry}/>
+                <WorldMap />
+                {this.state && this.state.selectedCountry &&
+                  <CountryInfo country={this.state.selectedCountry}/>
+                }
+                </Route>
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
