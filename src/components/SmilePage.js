4052db241440825c99be6bb1e19eb8f6e13911df
github username <github email address>
2020-03-26 22:56:07 -0600

    Getting features to work on smile page

--- src/components/SmilePage.js
+++ src/components/SmilePage.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import ShowSmile from './ShowSmile'
 import ShowPhoto from './ShowPhoto'
+import ShowJoke from './ShowJoke'
 export default class SmilePage extends Component{
     state={
         photoInfo:[],
-        thePhoto: null
+        thePhoto: null,
+        jokeInfo: [],
+        theJoke: null
     }
 
     componentDidMount(){
         fetch('https://picsum.photos/v2/list')
             .then(response => response.json())
             .then(photoInfo => this.setState({ photoInfo }))
+
+        fetch('https://sv443.net/jokeapi/category/programming')
+            .then(response =>response.json())
+            .then(jokeInfo => this.setState({jokeInfo}))
     }
     selectPhoto = (photo) => {
         this.setState({
@@ -18,14 +25,26 @@ export default class SmilePage extends Component{
         })
       }
     
+    selectJoke = (joke) =>{
+        this.setState({
+            theJoke: joke
+        })
+    }
+    reloadDocument = () => document.location.reload()
+
     render(){
         return(
             <div className="smile-page">
                 <h1>Smile</h1>
-                <ShowSmile photoInfo={this.state.photoInfo} selectPhoto={this.selectPhoto}/>
+                <ShowSmile photoInfo={this.state.photoInfo} jokeInfo={this.state.jokeInfo} 
+                selectJoke={this.selectJoke} selectPhoto={this.selectPhoto} reload={this.reloadDocument}/>
                 {this.state && this.state.thePhoto &&
                   <ShowPhoto photo={this.state.thePhoto}/>
                 }
+                {this.state && this.state.thePhoto &&
+                  <ShowJoke theJoke={this.state.theJoke}/>
+                }
+                
             </div>
         )
     }
